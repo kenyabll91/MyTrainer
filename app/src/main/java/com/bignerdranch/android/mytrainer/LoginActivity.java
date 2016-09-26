9e9205b5e179ee34f156a4b3cdebe93424e9d311
kenyabell <kenyabll91@gmail.com>
2016-09-26 15:05:36 -0400

    week 5 new changes

--- app/src/main/java/com/bignerdranch/android/mytrainer/LoginActivity.java
+++ app/src/main/java/com/bignerdranch/android/mytrainer/LoginActivity.java
@@ -3,8 +3,11 @@ package com.bignerdranch.android.mytrainer;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
@@ -40,25 +43,20 @@ import static android.Manifest.permission.READ_CONTACTS;
 
 public class LoginActivity extends FragmentActivity {
 
-    public class Logout extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            getMenuInflater().inflate(R.menu.main_menu_options, menu);
-            return true;
-        }
+        Button login = (Button)findViewById(R.id.bLogin);
+        login.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, CustomerListActivity.class);
+                startActivity(intent);
 
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.logout_settings:
-                    return true;
-                default:
-                    return super.onOptionsItemSelected(item);
             }
-
-
-        }
+        });
     }
 }
 
